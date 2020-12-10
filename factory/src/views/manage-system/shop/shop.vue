e16d5482276387fc8f18117997a709e45ebaf864
bingzhe <874177815@qq.com>
2020-12-10 19:02:52 +0800

    查询

--- factory/src/views/manage-system/shop/shop.vue
+++ factory/src/views/manage-system/shop/shop.vue
@@ -4,7 +4,7 @@
     <div class="search-wrapper">
       <el-form ref="searchForm" :model="searchForm" :inline="true">
         <el-form-item label="状态" prop="status" label-width="70px">
-          <el-select v-model="searchForm.status" placeholder="请选择">
+          <el-select v-model="searchForm.status" placeholder="请选择" @change="getBusinessList">
             <el-option key="全部" label="全部" value />
             <el-option
               v-for="item in statusOptions"
@@ -15,7 +15,7 @@
           </el-select>
         </el-form-item>
         <el-form-item label="业务员" prop="salesman" label-width="70px">
-          <el-select v-model="searchForm.salesman" filterable placeholder="请选择">
+          <el-select v-model="searchForm.salesman" filterable placeholder="请选择" @change="getBusinessList">
             <el-option key="全部" label="全部" value />
             <el-option
               v-for="item in employee_list"
@@ -44,6 +44,7 @@
             start-placeholder="开始日期"
             end-placeholder="结束日期"
             :default-time="['00:00:00', '23:59:59']"
+            @change="getBusinessList"
           />
         </el-form-item>
         <el-form-item>
@@ -186,8 +187,17 @@ export default {
   },
   mounted() {
     this.getBusinessList()
+    document.addEventListener('keydown', this.handleKeydownEvent, false)
+  },
+  beforeDestroy() {
+    document.removeEventListener('keydown', this.handleKeydownEvent, false)
   },
   methods: {
+    handleKeydownEvent(e) {
+      if (e.keyCode === 13) {
+        this.getBusinessList()
+      }
+    },
     async getBusinessList() {
       const data = {
         opr: 'get_business_list',
